8abf1d51313b21750d1107a9043ecc62fcb75ee3
eMobilis <chrispycharmprojects@gmail.com>
2023-02-10 18:34:23 +0300

    function statements

---
+++ function/exercise.py
@@ -0,0 +1,6 @@
+def calculateage():
+    Age = int(input("Enter your age"))
+    Result =Age*12
+    print(f"You have lived for {Result} months")34
+
+calculateage()
---
+++ main.py
@@ -0,0 +1,4 @@
+def sayhello():
+    print("hello user")
+    
+sayhello()
